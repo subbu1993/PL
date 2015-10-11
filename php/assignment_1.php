52397831228532f484713ad78a83053a252df437
raghavrav4 <rr2731@nyu.edu>
2015-10-11 14:18:51 -0500

    comments done

--- php/assignment_1.php
+++ php/assignment_1.php
@@ -7,16 +7,16 @@
     echo "php filename.php task_number input.txt output.txt\n";
     exit(1);
   }
-  // opening the input file
-  $file_stream = fopen($argv[2],'r');
-  $file_write = fopen($argv[3],'a');
+
+  $file_stream = fopen($argv[2],'r'); // opening the input file
+  $file_write = fopen($argv[3],'a'); // file stream to write out the output file
   if(!$file_stream)
   {
     echo ("Sorry could not open the file");
     exit(1);
   }
 
-  if($argv[1] == 2)
+  if($argv[1] == 2) // setting up the map for task 2
   {
     $map = array('a' => 'q','C' => 'Z', 'D' => 'c', 'y' => 'Y');
   }
@@ -44,37 +44,39 @@
   }
 
 function decryptCharacter($input_character)
-{
+{ // function that takes in a character and decodes it
   $ascii_value = ord($input_character);
   if($ascii_value >= 65 && $ascii_value <= 122)
-  {
+  { // all small and capital letters
     if($ascii_value >= 65 && $ascii_value <= 90)
-    {
+    { // all uppercase letters
     $ascii_value += 13;
       if($ascii_value > 90)
-      {
+      {// wrap around
         $ascii_value = $ascii_value - 90;
         $ascii_value += 65 + $ascii_value - 1;
       }
     }
-    elseif ($ascii_value >=97 && $ascii_value <= 122) {
+    elseif ($ascii_value >=97 && $ascii_value <= 122)
+    {// all lowercase letters
       $ascii_value += 13;
       if($ascii_value > 122)
-      {
+      { // wrap around
         $ascii_value = $ascii_value - 122;
         $ascii_value += 97 + $ascii_value - 1;
       }
     }
-    else {
+    else
+    { // any other character between 90-97
       return $input_character;
     }
     return chr($ascii_value);
   }
   elseif($ascii_value >= 48 && $ascii_value <= 57)
-  {
+  { // all numbers
     $ascii_value += 5;
     if($ascii_value > 57)
-    {
+    { // wrap around
       $ascii_value -= 57;
       $ascii_value = 48 + $ascii_value - 1;
     }
